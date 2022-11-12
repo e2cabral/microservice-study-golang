3edc86724b5b7a32d1674a41eb4e1aca797c5ec6
Cabral Junior <e2cabraljunior@gmail.com>
2022-11-12 16:56:36 -0300

    🚧 implements the routing system

--- src/main.go
+++ src/main.go
@@ -1,1 +1,17 @@
-package src
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"microservice-golang/src/main/config"
+)
+
+func main() {
+	app := gin.Default()
+
+	config.StartRouting(app)
+
+	if err := app.Run(":3000"); err != nil {
+		log.Fatal(err)
+	}
+}
