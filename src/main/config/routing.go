687d86521179b9637dcafee3f122e5c74ec04112
Cabral Junior <e2cabraljunior@gmail.com>
2022-11-12 16:54:51 -0300

    🚧 implement the facade function

--- src/main/config/routing.go
+++ src/main/config/routing.go
@@ -1,1 +1,10 @@
 package config
+
+import (
+	"github.com/gin-gonic/gin"
+	"microservice-golang/src/infra/facades"
+)
+
+func StartRouting(router *gin.Engine) {
+	facades.StartRoutingSystem(router)
+}
