d7ac7994d3501c1136060cce135c32bb548dc0ad
Cabral Junior <e2cabraljunior@gmail.com>
2022-11-12 16:53:06 -0300

    🚧 create a function to set product's routes

--- src/main/routes/products.go
+++ src/main/routes/products.go
@@ -1,1 +1,20 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"microservice-golang/src/infra/helpers"
+	"net/http"
+)
+
+func SetProductsRoutes(app *gin.Engine) {
+	router := helpers.NewRouter("/products")
+
+	products := app.Group(router.Group)
+	{
+		products.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Hello, World!",
+			})
+		})
+	}
+}
