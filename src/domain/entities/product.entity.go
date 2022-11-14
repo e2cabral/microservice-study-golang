c85d592c525c7c855dc80ebc7570a4bbda12e9e0
Cabral Junior <e2cabraljunior@gmail.com>
2022-11-14 12:42:17 -0300

    🚧 create a product model

---
+++ src/domain/entities/product.entity.go
@@ -0,0 +1,13 @@
+package entities
+
+import "gorm.io/gorm"
+
+type Product struct {
+	gorm.Model
+	Name        string     `gorm:"type:varchar" json:"name"`
+	Description string     `gorm:"type:varchar" json:"description"`
+	Category    []Category `gorm:"many2many:products_categories" json:"category"`
+	Reviews     []Review   `gorm:"foreignKey:ProductID" json:"reviews"`
+	ID          uint       `gorm:"primaryKey; type:int" json:"id"`
+	Price       float64    `gorm:"type:float" json:"price"`
+}
