1f7a9830a14d0451e750458d6edd7294f39aa729
Cabral Junior <e2cabraljunior@gmail.com>
2022-11-14 12:43:55 -0300

    ✨ add a database client to connect to the database

---
+++ src/infra/database/database.go
@@ -0,0 +1,61 @@
+package database
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"microservice-golang/src/domain/entities"
+	"microservice-golang/src/infra/helpers"
+)
+
+func Connect() (*gorm.DB, error) {
+	env := helpers.Env{}
+	host, err := env.GetVariable("DB_HOST")
+	user, err := env.GetVariable("DB_USER")
+	password, err := env.GetVariable("DB_PASSWORD")
+	dbname, err := env.GetVariable("DB_NAME")
+	port, err := env.GetVariable("DB_PORT")
+
+	if err != nil {
+		return nil, err
+	}
+
+	connectionString := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s",
+		host, user, password, dbname, port,
+	)
+
+	connection, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return connection, nil
+}
+
+func Migrate(database *gorm.DB) error {
+	err := database.AutoMigrate(
+		entities.Product{},
+		entities.Review{},
+		entities.Category{},
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func InitializeDatabaseConnection() error {
+	database, err := Connect()
+	if err != nil {
+		return err
+	}
+
+	if err := Migrate(database); err != nil {
+		return err
+	}
+
+	return nil
+}
