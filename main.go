b4a7f06b0e73075a5d80a9bfdb5ccccc825d2998
Cabral Junior <e2cabraljunior@gmail.com>
2022-11-14 12:44:46 -0300

    🚧 implements the database client connection

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"log"
+	"microservice-golang/src/infra/database"
 	"microservice-golang/src/main/config"
 )
 
 func main() {
+	if err := database.InitializeDatabaseConnection(); err != nil {
+		log.Fatal(err.Error())
+	}
 	app := gin.Default()
 
 	config.StartRouting(app)
