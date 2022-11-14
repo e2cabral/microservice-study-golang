cfa7fd56679a7ef97b41ddde81a26510395fb6c8
Cabral Junior <e2cabraljunior@gmail.com>
2022-11-14 19:42:46 -0300

    🚧 add a new method in the controller to retrieve the products

--- src/presentation/controllers/products.controller.go
+++ src/presentation/controllers/products.controller.go
@@ -5,6 +5,7 @@ import (
 	"microservice-golang/src/domain/services"
 	"microservice-golang/src/infra/helpers"
 	"net/http"
+	"strconv"
 )
 
 func Create(w http.ResponseWriter, r *http.Request) {
@@ -23,4 +24,27 @@ func Create(w http.ResponseWriter, r *http.Request) {
 	}
 
 	helpers.Ok(w, &product)
+}
+
+func Find(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+
+	current, err := strconv.Atoi(query.Get("current"))
+	if err != nil {
+		helpers.InternalServerError(w, err.Error())
+	}
+
+	limit, err := strconv.Atoi(query.Get("limit"))
+	if err != nil {
+		helpers.InternalServerError(w, err.Error())
+	}
+
+	service := services.ProductService{}
+
+	products, err := service.Find(current, limit)
+	if err != nil {
+		helpers.InternalServerError(w, err.Error())
+	}
+
+	helpers.Ok(w, products)
 }
