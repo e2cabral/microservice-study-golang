c2165968bcced1d1a72774c336a4fdd4b8890b9a
Cabral Junior <e2cabraljunior@gmail.com>
2022-11-14 17:46:45 -0300

    🚧 create a json helper

---
+++ src/infra/helpers/json.helper.go
@@ -0,0 +1,21 @@
+package helpers
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+func ToJSON(w http.ResponseWriter, data interface{}) {
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func FromJSON(r io.ReadCloser, data interface{}) error {
+	if err := json.NewDecoder(r).Decode(&data); err != nil {
+		return err
+	}
+	return nil
+}
