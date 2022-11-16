958b17fb225aa9c2efef0b81e2f79df92e42da55
Cabral Junior <e2cabraljunior@gmail.com>
2022-11-16 12:56:53 -0300

    🚧 create a method in the controller to update the products

--- src/presentation/controllers/products.controller.go
+++ src/presentation/controllers/products.controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"github.com/gorilla/mux"
 	"microservice-golang/src/domain/entities"
 	"microservice-golang/src/domain/services"
 	"microservice-golang/src/infra/helpers"
@@ -16,11 +17,13 @@ func Create(w http.ResponseWriter, r *http.Request) {
 	err := helpers.FromJSON(r.Body, &product)
 	if err != nil {
 		helpers.InternalServerError(w, err.Error())
+		return
 	}
 
 	err = service.Create(product)
 	if err != nil {
 		helpers.InternalServerError(w, err.Error())
+		return
 	}
 
 	helpers.Ok(w, &product)
@@ -32,11 +35,13 @@ func Find(w http.ResponseWriter, r *http.Request) {
 	current, err := strconv.Atoi(query.Get("current"))
 	if err != nil {
 		helpers.InternalServerError(w, err.Error())
+		return
 	}
 
 	limit, err := strconv.Atoi(query.Get("limit"))
 	if err != nil {
 		helpers.InternalServerError(w, err.Error())
+		return
 	}
 
 	service := services.ProductService{}
@@ -44,7 +49,34 @@ func Find(w http.ResponseWriter, r *http.Request) {
 	products, err := service.Find(current, limit)
 	if err != nil {
 		helpers.InternalServerError(w, err.Error())
+		return
 	}
 
 	helpers.Ok(w, products)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	var product entities.Product
+	vars := mux.Vars(r)
+
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		helpers.InternalServerError(w, err.Error())
+		return
+	}
+
+	service := services.ProductService{}
+
+	err = helpers.FromJSON(r.Body, &product)
+	if err != nil {
+		helpers.InternalServerError(w, err.Error())
+		return
+	}
+
+	err = service.Update(product, id)
+	if err != nil {
+		helpers.InternalServerError(w, err.Error())
+	}
+
+	helpers.Ok(w, product)
 }
